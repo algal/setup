8f2a5acae9df6aa42795f67d40a53adaee995d66
Alexis Gallagher <alexis@alexisgallagher.com>
2013-07-21 12:00:36 +0100

    bugfix and re-organzation.
    Bugfix. Added missing install python-software-properties, needed to
    call add-apt-repository for emacs24.
    
    Reorganized and commented installation commands.
    
    Added commented-out install command for mosh.

--- setup.sh
+++ setup.sh
@@ -2,10 +2,36 @@
 # Simple setup.sh for configuring Ubuntu 12.04 LTS EC2 instance
 # for headless setup. 
 
+# Update apt-get indexes
+sudo apt-get update -qq
+
+# Install basic dev utilities
+sudo apt-get install -y git curl python-software-properties
+
+#### mosh
+
+# Install mosh-server
+# # connect with mosh --ssh="ssh -i ~/.vagrant.d/insecure_private_key -p 2222" user@host
+# sudo add-apt-repository -y ppa:keithw/mosh
+# sudo apt-get -qq update
+# sudo apt-get install -y mosh
+
+#### emacs
+
+# Install emacs24
+# https://launchpad.net/~cassou/+archive/emacs
+sudo apt-add-repository -y ppa:cassou/emacs
+sudo apt-get -qq update
+sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
+
+#### node
+
+# Install rlwrap to provide libreadline features with node
+# See: http://nodejs.org/api/repl.html#repl_repl
+sudo apt-get install -y rlwrap
+
 # Install nvm: node-version manager
 # https://github.com/creationix/nvm
-sudo apt-get install -y git
-sudo apt-get install -y curl
 curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
 # Load nvm and install latest production node
@@ -17,19 +43,13 @@ nvm use v0.10.12
 # http://jshint.com/
 npm install -g jshint
 
-# Install rlwrap to provide libreadline features with node
-# See: http://nodejs.org/api/repl.html#repl_repl
-sudo apt-get install -y rlwrap
-
-# Install emacs24
-# https://launchpad.net/~cassou/+archive/emacs
-sudo apt-add-repository -y ppa:cassou/emacs
-sudo apt-get -qq update
-sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
+#### Heroku
 
 # Install Heroku toolbelt
 # https://toolbelt.heroku.com/debian
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
+#### dotfiles
 
 # git pull and install dotfiles as well
 cd $HOME
